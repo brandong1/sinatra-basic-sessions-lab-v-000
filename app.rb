eaab5adfb0cda45b15fc946d7466d430dc077212
Brandon Green <brandon@brandonleeproductions.com>
2018-10-25 00:54:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   end
 
   post '/checkout' do
-    session[:item] = params[:item]
+    session[:Items] = params[:Items]
     @session = session
     erb :checkout
   end
