b37a95689651cec62e85c5c83318b1c08d5a121e
Brandon Green <brandon@brandonleeproductions.com>
2018-10-25 00:45:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,9 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   configure do
-  enable :sessions
-  set :session_secret, "secret"
-end
+    enable :sessions
+    set :session_secret, "secret"
+  end
 
   get '/' do
     erb :index
